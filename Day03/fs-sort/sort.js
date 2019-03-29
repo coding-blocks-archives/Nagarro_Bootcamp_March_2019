5b1f0dfffe5f9b099c160d9cf62cf0dfbd9c979c
Arnav Gupta <arnav@codingblocks.com>
2019-03-29 13:27:24 +0530

    sort  complete
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day03/fs-sort/sort.js
+++ Day03/fs-sort/sort.js
@@ -4,23 +4,32 @@ const FIN1 = __dirname + '/in1.txt'
 const FIN2 = __dirname + '/in2.txt'
 const FOUT = __dirname + '/out.txt'
 
-fs.readFile(
-  FIN1,
-  (err, data) => {
+fs.readFile(FIN1, (err, data) => {
+  if (err) throw err
+
+  let nums = data.toString().split('\n')
+
+  fs.readFile(FIN2, (err, data) => {
     if (err) throw err
 
-    let nums = data.toString().split('\n')
+    nums = nums.concat(data.toString().split('\n'))
     nums.sort((a, b) => a - b)
-    console.log(nums)
 
-    fs.writeFile(
-      FOUT,
-      nums.join('\n'),
-      (err) => {
-        if (err) throw (err)
+    fs.writeFile(FOUT, nums.join('\n'), (err) => {
+      if (err) throw (err)
 
-        console.log('All Done!!')
-      }
-    )
-  }
-)
+      console.log('All Done!!')
+    })
+
+  })
+
+
+})
+
+
+async function task() {
+  let nums = await fs.readFile()
+  let nums2 = await fs.readFile()
+
+  await fs.writeFile()
+}
