94a9f815ee637efabde0f6b3c4c6ef6366ed96fb
Arnav Gupta <arnav@codingblocks.com>
2019-04-03 13:48:27 +0530

    assignment description
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day04/express-todolist/server.js
+++ Day04/express-todolist/server.js
@@ -21,7 +21,24 @@ app.post('/todos', (req, res) => {
     task: req.body.task,
     priority: parseInt(req.body.priority)
   })
-  res.send({success: true})
+  res.send({success: true, message: todos.length})
 })
 
 app.listen(7070)
+
+/**
+ * /todos -
+ *  GET fetch all todos
+ *  POST add a new todo
+ *
+ * /todos/:id -
+ *    GET - fetch single todo
+ *    PUT - replace a todo
+ *    PATCH - update data of a todo
+ *    DELETE - delete the todo
+ *
+ * Appropriate error messages for operations not possible
+ * Also send todos array length in response
+ *
+ * https://gist.github.com/championswimmer/2e3a309b880fdd2a9b12c1c3e0214bda
+ */
