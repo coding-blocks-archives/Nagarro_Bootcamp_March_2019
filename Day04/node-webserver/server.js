ca1406818cf33874d48132fed95b75824a4a1310
Arnav Gupta <arnav@codingblocks.com>
2019-04-03 11:46:32 +0530

    Express server app
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Day04/node-webserver/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/bye', (req, res) => {
+  res.send('Goodbye')
+})
+
+app.listen(8888)
