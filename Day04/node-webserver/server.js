9b7a47baf5e36758c5e16314d1faec14cfda7aa2
Arnav Gupta <arnav@codingblocks.com>
2019-04-03 12:15:27 +0530

    Express static and query params
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day04/node-webserver/server.js
+++ Day04/node-webserver/server.js
@@ -3,11 +3,35 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send(`
+  <html>
+  <head>
+    <title>My Awesome Page</title>
+  </head>
+  <body>
+    <h1>Hello !!</h1>
+    <p>Nice to see you</p>
+  </body>
+  </html>
+  `)
+})
+
+app.get('/hi', (req, res) => {
+  console.log(req.query)
+  if (req.query.name) {
+    res.send('Hello ' + req.query.name)
+  } else {
+    res.send('Hello Guest')
+  }
 })
 
 app.get('/bye', (req, res) => {
   res.send('Goodbye')
 })
+
+app.use(
+  '/pages',
+  express.static(__dirname + '/public')
+)
 
 app.listen(8888)
